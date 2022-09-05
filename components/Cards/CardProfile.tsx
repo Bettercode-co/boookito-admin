c41a7178f005c1562c9d4db9a66f8b2ba36d5b2f
Erfan Moghadasi <erfan.moqadasii@gmail.com>
2022-09-05 14:50:10 +0430

    fix: convert setting page  english to persian

--- components/Cards/CardProfile.tsx
+++ components/Cards/CardProfile.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import PN from "persian-number";
+
 
 const CardProfile: React.FC = () => {
   return (
@@ -19,58 +21,54 @@ const CardProfile: React.FC = () => {
               <div className="flex justify-center py-4 lg:pt-4 pt-8">
                 <div className="ml-4 p-3 text-center">
                   <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
-                    22
+                  {PN.convertEnToPe(22)}
                   </span>
-                  <span className="text-sm text-blueGray-400">Friends</span>
+                  <span className="text-sm text-blueGray-400">دوستان</span>
                 </div>
                 <div className="ml-4 p-3 text-center">
                   <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
-                    10
+                  {PN.convertEnToPe(10)}
                   </span>
-                  <span className="text-sm text-blueGray-400">Photos</span>
+                  <span className="text-sm text-blueGray-400">عکس ها</span>
                 </div>
                 <div className="lg:ml-4 p-3 text-center">
                   <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
-                    89
+                  {PN.convertEnToPe(89)}
                   </span>
-                  <span className="text-sm text-blueGray-400">Comments</span>
+                  <span className="text-sm text-blueGray-400">کامنت ها</span>
                 </div>
               </div>
             </div>
           </div>
           <div className="text-center mt-12">
             <h3 className="text-xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
-              Jenna Stones
+              فاطمه زهرایی
             </h3>
             <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
               <i className="fas fa-map-marker-alt ml-2 text-lg text-blueGray-400"></i>{" "}
-              Los Angeles, California
+              استان نهران، تهران
             </div>
             <div className="mb-2 text-blueGray-600 mt-10">
               <i className="fas fa-briefcase ml-2 text-lg text-blueGray-400"></i>
-              Solution Manager - Creative Tim Officer
+              برنامه نویس
             </div>
             <div className="mb-2 text-blueGray-600">
               <i className="fas fa-university ml-2 text-lg text-blueGray-400"></i>
-              University of Computer Science
+              دانشگاه تهران
             </div>
           </div>
           <div className="mt-10 py-10 border-t border-blueGray-200 text-center">
             <div className="flex flex-wrap justify-center">
               <div className="w-full lg:w-9/12 px-4">
                 <p className="mb-4 text-lg leading-relaxed text-blueGray-700">
-                  An artist of considerable range, Jenna the name taken by
-                  Melbourne-raised, Brooklyn-based Nick Murphy writes, performs
-                  and records all of his own music, giving it a warm, intimate
-                  feel with a solid groove structure. An artist of considerable
-                  range.
+                لورم ایپسوم مفهوم گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. 
                 </p>
                 <a
                   href="#pablo"
                   className="font-normal text-lightBlue-500"
                   onClick={(e) => e.preventDefault()}
                 >
-                  Show more
+                  نمایش بیشتر
                 </a>
               </div>
             </div>
--- components/Cards/CardSettings.tsx
+++ components/Cards/CardSettings.tsx
@@ -5,19 +5,19 @@ const CardSettings: React.FC = () => {
     <div className="relative lg:border-2 flex flex-col break-words w-full mb-6 shadow-lg rounded-lg bg-slate-100 ">
       <div className="rounded-t bg-white mb-0 px-6 py-6">
         <div className="text-center flex justify-between">
-          <h6 className="text-slate-700 text-xl font-bold">My account</h6>
+          <h6 className="text-slate-700 text-xl font-bold">تظیمات اکانت</h6>
           <button
             className="bg-slate-700 active:bg-slate-600 text-white font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none ml-1 ease-linear transition-all duration-150"
             type="button"
           >
-            Settings
+            تنظیمات
           </button>
         </div>
       </div>
       <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
         <form>
           <h6 className="text-slate-400 text-sm mt-3 mb-6 font-bold uppercase">
-            User Information
+            اطلاعات کاربر
           </h6>
           <div className="flex flex-wrap">
             <div className="w-full lg:w-6/12 px-4">
@@ -26,12 +26,12 @@ const CardSettings: React.FC = () => {
                   className="block uppercase text-slate-600 text-xs font-bold mb-2"
                   htmlFor="grid-password"
                 >
-                  Username
+                  نام کاربری
                 </label>
                 <input
                   type="text"
                   className="border-0 px-3 py-3 placeholder-slate-300 text-slate-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  defaultValue="lucky.jesse"
+                  defaultValue="فاطی"
                 />
               </div>
             </div>
@@ -41,12 +41,12 @@ const CardSettings: React.FC = () => {
                   className="block uppercase text-slate-600 text-xs font-bold mb-2"
                   htmlFor="grid-password"
                 >
-                  Email address
+                  آدرس ایمیل
                 </label>
                 <input
                   type="email"
                   className="border-0 px-3 py-3 placeholder-slate-300 text-slate-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  defaultValue="jesse@example.com"
+                  defaultValue="email@example.com"
                 />
               </div>
             </div>
@@ -56,12 +56,12 @@ const CardSettings: React.FC = () => {
                   className="block uppercase text-slate-600 text-xs font-bold mb-2"
                   htmlFor="grid-password"
                 >
-                  First Name
+                  نام
                 </label>
                 <input
                   type="text"
                   className="border-0 px-3 py-3 placeholder-slate-300 text-slate-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  defaultValue="Lucky"
+                  defaultValue="فاطمه"
                 />
               </div>
             </div>
@@ -71,12 +71,12 @@ const CardSettings: React.FC = () => {
                   className="block uppercase text-slate-600 text-xs font-bold mb-2"
                   htmlFor="grid-password"
                 >
-                  Last Name
+                  نام خانوادگی
                 </label>
                 <input
                   type="text"
                   className="border-0 px-3 py-3 placeholder-slate-300 text-slate-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  defaultValue="Jesse"
+                  defaultValue="زهرایی"
                 />
               </div>
             </div>
@@ -85,7 +85,7 @@ const CardSettings: React.FC = () => {
           <hr className="mt-6 border-b-1 border-slate-300" />
 
           <h6 className="text-slate-400 text-sm mt-3 mb-6 font-bold uppercase">
-            Contact Information
+            اطلاعات تماس
           </h6>
           <div className="flex flex-wrap">
             <div className="w-full lg:w-12/12 px-4">
@@ -94,12 +94,12 @@ const CardSettings: React.FC = () => {
                   className="block uppercase text-slate-600 text-xs font-bold mb-2"
                   htmlFor="grid-password"
                 >
-                  Address
+                  آدرس
                 </label>
                 <input
                   type="text"
                   className="border-0 px-3 py-3 placeholder-slate-300 text-slate-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  defaultValue="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                  defaultValue="استان تهران، تهران"
                 />
               </div>
             </div>
@@ -109,12 +109,12 @@ const CardSettings: React.FC = () => {
                   className="block uppercase text-slate-600 text-xs font-bold mb-2"
                   htmlFor="grid-password"
                 >
-                  City
+                  شهر
                 </label>
                 <input
                   type="email"
                   className="border-0 px-3 py-3 placeholder-slate-300 text-slate-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  defaultValue="New York"
+                  defaultValue="تهران"
                 />
               </div>
             </div>
@@ -124,12 +124,12 @@ const CardSettings: React.FC = () => {
                   className="block uppercase text-slate-600 text-xs font-bold mb-2"
                   htmlFor="grid-password"
                 >
-                  Country
+                  کشور
                 </label>
                 <input
                   type="text"
                   className="border-0 px-3 py-3 placeholder-slate-300 text-slate-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  defaultValue="United States"
+                  defaultValue="ایران"
                 />
               </div>
             </div>
@@ -139,12 +139,12 @@ const CardSettings: React.FC = () => {
                   className="block uppercase text-slate-600 text-xs font-bold mb-2"
                   htmlFor="grid-password"
                 >
-                  Postal Code
+                  کدپستی
                 </label>
                 <input
                   type="text"
                   className="border-0 px-3 py-3 placeholder-slate-300 text-slate-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  defaultValue="Postal Code"
+                  defaultValue="کدپستی"
                 />
               </div>
             </div>
@@ -153,7 +153,7 @@ const CardSettings: React.FC = () => {
           <hr className="mt-6 border-b-1 border-slate-300" />
 
           <h6 className="text-slate-400 text-sm mt-3 mb-6 font-bold uppercase">
-            About Me
+            درباره من
           </h6>
           <div className="flex flex-wrap">
             <div className="w-full lg:w-12/12 px-4">
@@ -162,13 +162,12 @@ const CardSettings: React.FC = () => {
                   className="block uppercase text-slate-600 text-xs font-bold mb-2"
                   htmlFor="grid-password"
                 >
-                  About me
+                             درباره من
                 </label>
                 <textarea
                   className="border-0 px-3 py-3 placeholder-slate-300 text-slate-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                   rows={4}
-                  defaultValue="A beautiful UI Kit and Admin for NextJS & Tailwind CSS. It is Free
-                    and Open Source."
+                  defaultValue=""
                 ></textarea>
               </div>
             </div>
