f0ede022f1de23b12e23211e239962033e1b43a1
Erfan Moghadasi <erfan.moqadasii@gmail.com>
2022-10-25 12:55:30 +0330

    feat: category page and add category modal

--- components/modals/CategoryModal.tsx
+++ components/modals/CategoryModal.tsx
@@ -40,76 +40,76 @@ const notifySuccess = () =>
   });
 
 const OrdresModal = ({ setIsModalOpen, isModalOpen }) => {
-//   const [newCategory, setNewCategory] = useState<NewOrder>({});
+  const [newCategory, setNewCategory] = useState<NewOrder>({});
 
-//   const {register, handleSubmit, formState: {errors}} = useForm<FormValues>()
+  const {register, handleSubmit, formState: {errors}} = useForm<FormValues>()
 
-//   const neworderHandler = () => {
-//     if (newCategory.categoryName) {
-//       axiosInstance
-//         .post("admin/categories", newCategory, {
-//           headers: {
-//             Authorization: `Bearer ${token}`,
-//           },
-//         })
-//         .then((res) => notifySuccess())
-//         .catch((err) => notifyError(err));
-//     } else {
-//       notifyError("خطا در ارسال سفارش");
-//     }
-//   };
+  const neworderHandler = () => {
+    if (newCategory.categoryName) {
+      axiosInstance
+        .post("admin/categories", newCategory, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((res) => notifySuccess())
+        .catch((err) => notifyError(err));
+    } else {
+      notifyError("خطا در ارسال سفارش");
+    }
+  };
 
 
-//   return (
-//     <>
-//       {isModalOpen && (
-//         <div
-//           className={`w-full h-full  flex justify-center items-center fixed t-0 r-0  bg-gray-300 bg-opacity-50 transition-all duration-300 ease-in`}
-//           onClick={() => setIsModalOpen(false)}
-//         >
-//           <form
-//           onSubmit={handleSubmit((data) => {setNewCategory(data)})}
-//             onClick={eventHandler}
-//             className="relative w-96 h-[40vh] rounded bg-white flex flex-col p-10  justify-around items-center"
-//           >
-//             <div
-//               onClick={() => setIsModalOpen(false)}
-//               className="absolute right-5 top-5 cursor-pointer"
-//             >
-//               <TiTimes size={20} />
-//             </div>
-//             <div className="w-full text-center">
-//             <h4>دسته بندی جدید</h4>
-//             <div className="h-[1px] bg-slate-200 w-full mt-5" />
-//             </div>
-//             <label className="w-full relative">
-//              نام دسته بندی
-//               <input
-//                 className="w-full border border-[#ccc] rounded h-[38px] mt-2"
-//                 {...register('categoryName', {required: 'نام دسته بندی را وارد کنید'})}
-//               />
-//                 {errors.categoryName && <p className="absolute -bottom-8 text-sm text-rose-600">{errors.categoryName.message}</p>}
-//             </label>
-//             <button
-//               type="submit"
-//               onClick={neworderHandler}
-//               className="w-full bg-slate-700 text-white h-10 rounded hover:bg-slate-600"
-//             >
-//               ثبت
-//             </button>
-//           </form>
-//         </div>
-//       )}
-//       <ToastContainer
-//         position="bottom-right"
-//         autoClose={5000}
-//         hideProgressBar={false}
-//         newestOnTop={false}
-//         closeOnClick
-//         rtl={true}
-//       />
-//     </>
-//   );
+  return (
+    <>
+      {isModalOpen && (
+        <div
+          className={`w-full h-full  flex justify-center items-center fixed t-0 r-0  bg-gray-300 bg-opacity-50 transition-all duration-300 ease-in`}
+          onClick={() => setIsModalOpen(false)}
+        >
+          <form
+          onSubmit={handleSubmit((data) => {setNewCategory(data)})}
+            onClick={eventHandler}
+            className="relative w-96 h-[40vh] rounded bg-white flex flex-col p-10  justify-around items-center"
+          >
+            <div
+              onClick={() => setIsModalOpen(false)}
+              className="absolute right-5 top-5 cursor-pointer"
+            >
+              <TiTimes size={20} />
+            </div>
+            <div className="w-full text-center">
+            <h4>دسته بندی جدید</h4>
+            <div className="h-[1px] bg-slate-200 w-full mt-5" />
+            </div>
+            <label className="w-full relative">
+             نام دسته بندی
+              <input
+                className="w-full border border-[#ccc] rounded h-[38px] mt-2"
+                {...register('categoryName', {required: 'نام دسته بندی را وارد کنید'})}
+              />
+                {errors.categoryName && <p className="absolute -bottom-8 text-sm text-rose-600">{errors.categoryName.message}</p>}
+            </label>
+            <button
+              type="submit"
+              onClick={neworderHandler}
+              className="w-full bg-slate-700 text-white h-10 rounded hover:bg-slate-600"
+            >
+              ثبت
+            </button>
+          </form>
+        </div>
+      )}
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={true}
+      />
+    </>
+  );
 };
 
 export default OrdresModal;
--- pages/admin/categories.tsx
+++ pages/admin/categories.tsx
@@ -12,102 +12,102 @@ import { IoIosAddCircleOutline } from "react-icons/io";
 
 
 
-const Categories = () => {
-  // const [categoriesData, setCategoriesData] = useState(null);
-  // const [isModalOpen, setIsModalOpen] = useState(false);
+const Categories: NextPage = () => {
+  const [categoriesData, setCategoriesData] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  // const moadalHandler = () => {
-  //   setIsModalOpen(!isModalOpen);
-  // };
+  const moadalHandler = () => {
+    setIsModalOpen(!isModalOpen);
+  };
 
-  // const fetchOrders = () => {
-  //   axiosInstance
-  //     .get(`admin/categories`)
-  //     .then((res) => setCategoriesData(res.data));
-  // };
+  const fetchOrders = () => {
+    axiosInstance
+      .get(`admin/categories`)
+      .then((res) => setCategoriesData(res.data));
+  };
 
-  // useEffect(() => {
-  //   fetchOrders();
-  // }, []);
+  useEffect(() => {
+    fetchOrders();
+  }, []);
 
-  // const COLUMNS = [
-  //   {
-  //     Header: "نام دسته بندی",
-  //     accessor: "categoryName",
-  //     minWidth: 50,
-  //   },
-  //   {
-  //     Header: "عملیات",
-  //     accessor: "action",
-  //     Cell: (cell) => (
-  //       <div className="flex items-center gap-3">
-  //         <button value={cell.accessor} className="felx items-center min-w-max" onClick={handleEdit}>
-  //         <span className="flex items-center bg-blue-500 px-[4px] rounded text-white  hover:text-blue-900 hover:bg-white">
+  const COLUMNS = [
+    {
+      Header: "نام دسته بندی",
+      accessor: "categoryName",
+      minWidth: 50,
+    },
+    {
+      Header: "عملیات",
+      accessor: "action",
+      Cell: (cell) => (
+        <div className="flex items-center gap-3">
+          <button value={cell.accessor} className="felx items-center min-w-max" onClick={handleEdit}>
+          <span className="flex items-center bg-blue-500 px-[4px] rounded text-white  hover:text-blue-900 hover:bg-white">
 
-  //             <RiEditFill /> &nbsp; ویرایش
-  //           </span>
-  //         </button>
-  //         <button value={cell.accessor} className="felx items-center min-w-max  cursor-pointer"  onClick={handleDelete}>
-  //         <span className="flex items-center bg-red-500 px-[4px] rounded text-white  hover:text-red-900 hover:bg-white ">
+              <RiEditFill /> &nbsp; ویرایش
+            </span>
+          </button>
+          <button value={cell.accessor} className="felx items-center min-w-max  cursor-pointer"  onClick={handleDelete}>
+          <span className="flex items-center bg-red-500 px-[4px] rounded text-white  hover:text-red-900 hover:bg-white ">
 
-  //             <MdDelete /> &nbsp; حذف
-  //           </span>
-  //         </button>
-  //       </div>
-  //     ),
-  //   },
-  // ];
+              <MdDelete /> &nbsp; حذف
+            </span>
+          </button>
+        </div>
+      ),
+    },
+  ];
 
-  // const handleEdit = (e) => {
-  //   console.log("edit button");
-  // };
-  // const handleDelete = () => {
-  //   console.log("delete button");
-  // };
+  const handleEdit = (e) => {
+    console.log("edit button");
+  };
+  const handleDelete = () => {
+    console.log("delete button");
+  };
 
-  // if (!categoriesData) {
-  //   return <div className="h-[80vh] w-full mx-auto flex items-center justify-center">
-  //   <svg
-  //     className="animate-spin h-14 w-14 mx-auto text-gray-600"
-  //     xmlns="http://www.w3.org/2000/svg"
-  //     fill="none"
-  //     viewBox="0 0 24 24"
-  //   >
-  //     <circle
-  //       className="opacity-25"
-  //       cx={12}
-  //       cy={12}
-  //       r={10}
-  //       stroke="currentColor"
-  //       strokeWidth={4}
-  //     />
-  //     <path
-  //       className="opacity-75"
-  //       fill="currentColor"
-  //       d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-  //     />
-  //   </svg>
-  // </div>;
-  // }
-  // return (
-  //   <div className="pt-14">
-  //     <div className="fixed top-0 right-0 z-50">
-  //       <CategoryModal
-  //         isModalOpen={isModalOpen}
-  //         setIsModalOpen={setIsModalOpen}
-  //       />
-  //     </div>
-  //     <div className=" flex justify-end">
-  //       <button
-  //         onClick={moadalHandler}
-  //         className=" left-6 flex items-center justify-center   w-32 h-10 rounded bg-cyan-700 text-white hover:bg-cyan-600 transition "
-  //       >
-  //         <IoIosAddCircleOutline /> اضافه کردن
-  //       </button>
-  //     </div>
-  //     <BasicTable rowsdata={categoriesData} columnsData={COLUMNS} />
-  //   </div>
-  // );
+  if (!categoriesData) {
+    return <div className="h-[80vh] w-full mx-auto flex items-center justify-center">
+    <svg
+      className="animate-spin h-14 w-14 mx-auto text-gray-600"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <circle
+        className="opacity-25"
+        cx={12}
+        cy={12}
+        r={10}
+        stroke="currentColor"
+        strokeWidth={4}
+      />
+      <path
+        className="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      />
+    </svg>
+  </div>;
+  }
+  return (
+    <div className="pt-14">
+      <div className="fixed top-0 right-0 z-50">
+        <CategoryModal
+          isModalOpen={isModalOpen}
+          setIsModalOpen={setIsModalOpen}
+        />
+      </div>
+      <div className=" flex justify-end">
+        <button
+          onClick={moadalHandler}
+          className=" left-6 flex items-center justify-center   w-32 h-10 rounded bg-cyan-700 text-white hover:bg-cyan-600 transition "
+        >
+          <IoIosAddCircleOutline /> اضافه کردن
+        </button>
+      </div>
+      <BasicTable rowsdata={categoriesData} columnsData={COLUMNS} />
+    </div>
+  );
 };
 
 (Categories as any).layout = Admin;
