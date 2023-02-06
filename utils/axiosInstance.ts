4265b113eff070198fa003a469d96e50fb7e85b9
tahatech <thhk2831@gmail.com>
2023-02-06 10:46:36 -0800

    .

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
  const axiosInstance = axios.create({
     baseURL: "https://api.boookito.ir/api/v2/",
+    // baseURL: "http://localhost:5000/api/v2/",
     headers:{
         Accept: "application/json"
     }
