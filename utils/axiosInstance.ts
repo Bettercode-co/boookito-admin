6a2cb35dda4a38c34ea7b02a1a31dff8bd605b1d
Mohammad Hasani <m789219@gmail.com>
2023-04-16 16:34:25 +0330

    add loadbalance and ci cd

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
+const backendInstance=['https://api.boookito.ir/api/v2/',"https://api2.boookito.ir/api/v2/"]
+ let randomNumber=Math.floor(Math.random() * 10) +1
+    let finalInstance=randomNumber>0 && randomNumber<6 ? 0 : 1
 
  const axiosInstance = axios.create({
-    baseURL: "https://api.boookito.ir/api/v2/",
+    baseURL: backendInstance[finalInstance],
     // baseURL: "http://localhost:5000/api/v2/",
     headers:{
         Accept: "application/json"
