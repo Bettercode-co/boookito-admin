7afe2b98c03647e1c271e8fb6635a5dd282f67e5
MohammadHasani <m789219@gmail.com>
2023-05-21 12:57:31 +0430

    change instanse api

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
  const axiosInstance = axios.create({
-    baseURL: "https://getaway.boookito.ir/api/v2",
+    baseURL: "https://api.boookito.ir/api/v2",
     // baseURL: "http://192.168.163.67:5000/api/v2/",
     headers:{
         Accept: "application/json"
